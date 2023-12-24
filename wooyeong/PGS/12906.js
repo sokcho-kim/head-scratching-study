d066a82d9ef0f4a800e67eacb99ad70ea194b628
uuyeong <proyeongs@gmail.com>
2023-12-24 10:07:54 +0900

    [PGS] (스택/큐) 같은 숫자는 싫어  / LEVEL1  / 10분 / #2
    https://school.programmers.co.kr/learn/courses/30/lessons/12906

---
+++ wooyeong/PGS/12906.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+  const answer = [arr[0]];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1]) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
+
+// function solution(arr) {
+//   const answer = arr.filter((ele, i) => arr[i + 1] !== ele);
+//   return answer;
+// }
