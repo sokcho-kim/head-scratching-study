12d18953ec868f673502c359a2653b532334f3e7
uuyeong <proyeongs@gmail.com>
2024-01-03 23:09:16 +0900

    [LTC] Dl String Match / Easy /#8

--- wooyeong/LTC/942.js
+++ wooyeong/LTC/942.js
@@ -3,18 +3,36 @@
  * @return {number[]}
  */
 // Todo : two pointer
+// var diStringMatch = function (s) {
+//   let i = 0;
+//   let samples = Array.from({ length: s.length + 1 }, () => i++);
+//   const result = [];
+//   for (let char of s) {
+//     if (char === "I") {
+//       const min = samples.shift();
+//       result.push(min);
+//     } else {
+//       const max = samples.pop();
+//       result.push(max);
+//     }
+//   }
+//   return [...result, ...samples];
+// };
+
 var diStringMatch = function (s) {
   let i = 0;
   let samples = Array.from({ length: s.length + 1 }, () => i++);
+
+  let lpt = 0;
+  let rpt = samples.length - 1;
+
   const result = [];
   for (let char of s) {
     if (char === "I") {
-      const min = samples.shift();
-      result.push(min);
+      result.push(samples[lpt++]);
     } else {
-      const max = samples.pop();
-      result.push(max);
+      result.push(samples[rpt--]);
     }
   }
-  return [...result, ...samples];
+  return [...result, samples[lpt]];
 };
